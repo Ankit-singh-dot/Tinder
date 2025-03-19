bf67938b22b3249b31d3c1609c09dc113486ba1f
Ankit <ankitrajdihara123@gmail.com>
2025-03-20 02:19:45 +0530

    exploring get,post and etc

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app = express();
-app.use("/namaste",(req, res) => {
-  res.send("this is me ");
+// app.use("/hello/2", (req, res) => {
+//   res.send("this is me batman");
+// });
+app.get("/hello", (req, res) => {
+  res.send({ firstName: "Ankit", SecondName: "Singh" });
 });
-app.use("/never",(req, res) => {
-  res.send("always");
+app.post("/hello", (req, res) => {
+  res.send("data saved successfully");
 });
-app.use("/hello",(req, res) => {
-  res.send("hello hello hello ");
+app.use("/hello", (req, res) => {
+  res.send("this is me superman");
 });
-app.listen(3000, () => {
-  console.log("listening to the port 3000");
+app.listen(8000, () => {
+  console.log("listening to the port of 8000");
 });
