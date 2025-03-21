288e021fd47d904e3b0a7e69654600311dfa65ea
Ankit <ankitrajdihara123@gmail.com>
2025-03-21 12:24:59 +0530

    database_learning

--- src/app.js
+++ src/app.js
@@ -1,22 +1,13 @@
 const express = require("express");
+const connectDB = require("./config/database");
 const app = express();
-// app.use("/hello/2", (req, res) => {
-//   res.send("this is me batman");
-// });
-// app.get("/hello", (req, res) => {
-//   res.send({ firstName: "Ankit", SecondName: "Singh" });
-// });
-// app.post("/hello", (req, res) => {
-//   res.send("data saved successfully");
-// });
-app.get("/hello", (req, res) => {
-    console.log("this is me superman");
-  res.send("this is me superman");
-});
-app.get("/hello/madam", (req, res) => {
-    console.log("this is me ");
-  res.send("this is me ");
-});
-app.listen(8000, () => {
-  console.log("listening to the port of 8000");
-});
+connectDB()
+  .then((result) => {
+    console.log("db connect successfully");
+    app.listen(8000, () => {
+      console.log("SAY my name");
+    });
+  })
+  .catch((err) => {
+    console.error("db not connected successfully");
+  });
