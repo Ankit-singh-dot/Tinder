e2e36efe384737f53bc18f3b29b17c4d33a30769
Ankit <ankitrajdihara123@gmail.com>
2025-03-22 11:18:54 +0530

    postman_shit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const User = require("./models/user");
 const connectDB = require("./config/database");
+
 const app = express();
+app.use(express.json()); 
+app.post("/signup", async (req, res) => {
+ 
+  // creating a user instance of user model
+  const user = new User(
+    {firstName: "ankit",
+    lastName: "singh",
+    emailId: "hat_na_laude",}
+  );
+  await user.save();
+  res.send("user successfully");
+});
 connectDB()
   .then((result) => {
     console.log("db connect successfully");
